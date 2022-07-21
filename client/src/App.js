4a0ab4e47fff2d6f778c110f5f75880395b11887
satwider47 <satwinder47@outlook.com>
2022-07-21 17:54:49 -0400

    added folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, from 'react';
 import Nav from './components/Nav';
 // import About from './components/About';
 // import Resume from './components/Resume';
