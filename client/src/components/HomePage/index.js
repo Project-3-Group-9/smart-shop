11e510c83010568ab7959af760197f3433d85949
satwider47 <satwinder47@outlook.com>
2022-07-21 20:25:48 -0400

    added homepage links

---
+++ client/src/components/HomePage/index.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react';
+import { NavLink,Link } from 'react-router-dom';
+function Homepage() {
+    return(
+    <section>satwinder</section>
+    )
+}
+export default Homepage;
