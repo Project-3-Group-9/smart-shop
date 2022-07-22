a07963d2c00f8af25fae5b01299872609ac87257
satwider47 <satwinder47@outlook.com>
2022-07-21 21:43:05 -0400

    folders structure

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import HomePage from './components/HomePage';
 // import Resume from './components/Resume';
 // import Contact from './components/Contact';
 // import Project from './components/Project';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
   <Route path="/contact" element={<Contact/>}/>
   <Route path="/resume" element={<Resume/>}/> */}
     </Routes>
-    {/* <Footer/> */}
+    <Footer/>
     </div>
     </Router>
   );
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope,faSquarePhone,faCodeCompare,faUser} from '@fortawesome/free-solid-svg-icons'
+function Footer() {
+    return(
+<footer className="p-2 p-md-5 bg-dark">
+
+      <address style={{fontSize:"20px"}} >
+        <ul className=" d-md-flex justify-content-between list-unstyled">
+        <li><a href="mailto:satwinder47@outlook.com" className="text-white text-decoration-none">
+        Email@outlook.com
+          </a></li>
+        <li><a href="tel:2222222222" className="text-white text-decoration-none">
+        (289) 222-222</a></li>
+        <li><a href="" className="text-white text-decoration-none">
+        Git-hub</a></li>
+        <li><a href="" className="text-white text-decoration-none"> 
+        Heroku</a></li>
+        </ul>
+      </address>
+      <div className="text-center">Thanks for visiting our site</div>
+      <div className="text-center">&copy; Smart Shop inc.</div>
+  </footer>
+    )
+}
+
+export default Footer;
