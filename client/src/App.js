1f2afc766c308686987ceed36c3e67615241337e
TahirMurtaza <abdulessa93@gmail.com>
2022-07-27 22:55:57 +0500

    graphql link changed for deployement

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,10 @@ import Footer from './components/Footer';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIN from './components/SignInPage';
 import SignUp from './components/SignUpPage';
-const PORT = process.env.PORT || 3001;
+
 function App() {
   const httpLink = createHttpLink({
-    uri: `http://localhost:${PORT}/graphql`,
+    uri: `/graphql`,
   });
   
   const client = new ApolloClient({
